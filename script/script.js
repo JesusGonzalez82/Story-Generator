28e8073dd6107440009a1052efc1364c53ee77f2
Jesus Gonzalez <jgblanco82@yahoo.es>
2024-12-30 17:52:51 +0100

    mas modificaciones v2

--- script/script.js
+++ script/script.js
@@ -141,9 +141,19 @@ function crearHistoriaMontarazO(sexo){
     texto.innerHTML = historiaMontaraz.join(" ");
 }
 
-function crearHistoriaHobbit(){
+function crearHistoriaHobbit(sexo){
+    if (sexo === 'masculino'){
+        crearHistoriaHobbitM();
+    }else if (sexo === 'femenino') {
+        crearHistoriaHobbitF();
+    }else {
+        crearHistoriaHobbitO();
+    }
+}
+
+function crearHistoriaHobbitM(){
     texto.innerHTML = "";
-    let nombre = document.getElementById('nombre').value.toString();
+    let nombre = document.getElementById('nombre').value.trim();
 
     if (!nombre){
         texto.innerHTML = "Por favor, ingresa un nombre para nuestro héroe";
@@ -169,9 +179,76 @@ let historiaHobbit = [
     texto.innerHTML = historiaHobbit.join(" ");
 }
 
-function crearHistoriaElfo(){
+function crearHistoriaHobbitF(){
     texto.innerHTML = "";
-    let nombre = document.getElementById('nombre').value.toString();
+    let nombre = document.getElementById('nombre').value.trim();
+
+    if (!nombre){
+        texto.innerHTML = "Por favor, ingresa un nombre para nuestro héroe";
+        return;
+    }
+
+    let historiaHobbit = [
+        "<h1>Venturas y desventuras de " + nombre + " la hobbit!</h1>",
+        "En la tranquila Comarca, " + nombre + ", una joven hobbit llena de curiosidad,",
+        "decidió que ya no podía seguir viviendo una vida monótona. Aunque su gente prefería la paz y la seguridad de su hogar,",
+        "algo dentro de ella le decía que había algo más allá de los campos verdes. Un día, un mago llamado Gandalf apareció en su puerta",
+        "y le ofreció unirse a un grupo de enanos en una aventura hacia la Montaña Solitaria.",
+        "Aunque dudosa, " + nombre + " aceptó la propuesta, y así comenzó su gran viaje.",
+        "A lo largo del camino, " + nombre + " se enfrentó a criaturas desconocidas y peligros imprevistos. Pero su valentía crecía con cada desafío,",
+        "y aunque pequeña en estatura, su coraje la hizo grande en el corazón. En un momento crucial, " + nombre + " encontró un anillo mágico que",
+        "le permitió volverse invisible, lo cual fue clave para derrotar a un grupo de orcos.",
+        "Tras muchos días de lucha, " + nombre + " y los enanos llegaron a la Montaña Solitaria, donde se enfrentaron al dragón Smaug.",
+        "Al final, con astucia y valentía, " + nombre + " recuperó el tesoro y regresó a la Comarca,",
+        "donde, aunque cambió para siempre, siempre llevaba consigo el recuerdo de su épica aventura."
+    ];
+
+    
+    texto.innerHTML = historiaHobbit.join(" ");
+}
+
+function crearHistoriaHobbitO(){
+    texto.innerHTML = "";
+    let nombre = document.getElementById('nombre').value.trim();
+
+    if (!nombre){
+        texto.innerHTML = "Por favor, ingresa un nombre para nuestro héroe";
+        return;
+    }
+
+
+    let historiaHobbit = [
+        "<h1>Venturas y desventuras de " + nombre + " le hobbit!</h1>",
+        "En la tranquila Comarca, " + nombre + ", une joven hobbit llene de curiosidad,",
+        "decidió que ya no podía seguir viviendo una vida monótona. Aunque su gente prefería la paz y la seguridad de su hogar,",
+        "algo dentro de elle le decía que había algo más allá de los campos verdes. Un día, un mago llamado Gandalf apareció en su puerta",
+        "y le ofreció unirse a un grupo de enanos en una aventura hacia la Montaña Solitaria.",
+        "Aunque dudose, " + nombre + " aceptó la propuesta, y así comenzó su gran viaje.",
+        "A lo largo del camino, " + nombre + " se enfrentó a criaturas desconocidas y peligros imprevistos. Pero su valentía crecía con cada desafío,",
+        "y aunque pequeñe en estatura, su coraje le hizo grande en el corazón. En un momento crucial, " + nombre + " encontró un anillo mágico que",
+        "le permitió volverse invisible, lo cual fue clave para derrotar a un grupo de orcos.",
+        "Tras muchos días de lucha, " + nombre + " y los enanos llegaron a la Montaña Solitaria, donde se enfrentaron al dragón Smaug.",
+        "Al final, con astucia y valentía, " + nombre + " recuperó el tesoro y regresó a la Comarca,",
+        "donde, aunque cambió para siempre, siempre llevaba consigo el recuerdo de su épica aventura."
+    ];
+
+    
+    texto.innerHTML = historiaHobbit.join(" ");
+}
+
+function crearHistoriaElfo(sexo){
+    if (sexo === 'masculino'){
+        crearHistoriaElfoM();
+    }else if (sexo === 'femenino') {
+        crearHistoriaElfoF();
+    }else {
+        crearHistoriaElfoO();
+    }
+}
+
+function crearHistoriaElfoM(){
+    texto.innerHTML = "";
+    let nombre = document.getElementById('nombre').value.trim();
 
     if (!nombre){
         texto.innerHTML = "Por favor, ingresa un nombre para nuestro héroe";
@@ -197,7 +274,73 @@ let historiaElfo = [
     texto.innerHTML = historiaElfo.join(" ");
 }
 
-function crearHistoriaEnano(){
+function crearHistoriaElfoF(){
+    texto.innerHTML = "";
+    let nombre = document.getElementById('nombre').value.trim();
+
+    if (!nombre){
+        texto.innerHTML = "Por favor, ingresa un nombre para nuestro héroe";
+        return;
+    }
+
+    let historiaElfo = [
+        "<h1>" + nombre + " La guardiana de Lórien </h1>",
+        "En el reino oculto de Lórien, " + nombre + ", una noble elfa de la casa de Galendil,",
+        "era conocida por su destreza con el arco y su profunda conexión con la naturaleza.",
+        "Su cabello dorado brillaba bajo la luz de la luna, y sus ojos azules reflejaban siglos de sabiduría.",
+        "Una noche, mientras las estrellas iluminaban el cielo, " + nombre + " recibió una visión perturbadora:",
+        "un oscuro poder estaba despertando en las profundidades de las Montañas Nubladas.",
+        "Sin dudarlo, " + nombre + " partió hacia la montaña acompañada de su fiel águila, Aeranor.",
+        "En el camino, encontró un grupo de trolls que habían capturado a viajeros inocentes.",
+        "Con movimientos elegantes y precisos, " + nombre + " liberó a los prisioneros y acabó con los trolls.",
+        "Finalmente, al llegar a las Montañas Nubladas, descubrió una antigua fortaleza llena de espectros.",
+        "Con la luz de una joya élfica, dispersó la oscuridad y selló el portal que los espectros usaban.",
+        "Regresó a Lórien como una heroína silenciosa, llevando consigo no gloria, sino paz para su tierra.",
+        "Para " + nombre + ", la verdadera victoria era proteger el equilibrio del mundo."
+    ];
+    
+    texto.innerHTML = historiaElfo.join(" ");
+}
+
+function crearHistoriaElfoO(){
+    texto.innerHTML = "";
+    let nombre = document.getElementById('nombre').value.trim();
+
+    if (!nombre){
+        texto.innerHTML = "Por favor, ingresa un nombre para nuestro héroe";
+        return;
+    }
+
+    let historiaElfo = [
+        "<h1>" + nombre + " Le guardián de Lórien </h1>",
+        "En el reino oculto de Lórien, " + nombre + ", une noble elfe de la casa de Galendil,",
+        "era conocide por su destreza con el arco y su profunda conexión con la naturaleza.",
+        "Su cabello dorado brillaba bajo la luz de la luna, y sus ojos azules reflejaban siglos de sabiduría.",
+        "Una noche, mientras les estrellas iluminaban el cielo, " + nombre + " recibió una visión perturbadora:",
+        "un oscuro poder estaba despertando en las profundidades de las Montañas Nubladas.",
+        "Sin dudarlo, " + nombre + " partió hacia la montaña acompañade de su fiel águila, Aeranor.",
+        "En el camino, encontró un grupo de trolls que habían capturado a viajeros inocentes.",
+        "Con movimientos elegantes y precisos, " + nombre + " liberó a les prisioneres y acabó con los trolls.",
+        "Finalmente, al llegar a las Montañas Nubladas, descubrió una antigua fortaleza llena de espectros.",
+        "Con la luz de una joya élfica, dispersó la oscuridad y selló el portal que los espectros usaban.",
+        "Regresó a Lórien como une héroe silenciose, llevando consigo no gloria, sino paz para su tierra.",
+        "Para " + nombre + ", la verdadera victoria era proteger el equilibrio del mundo."
+    ];
+    
+    texto.innerHTML = historiaElfo.join(" ");
+}
+
+function crearHistoriaEnano(sexo){
+    if (sexo === 'masculino'){
+        crearHistoriaEnanoM();
+    }else if (sexo === 'femenino') {
+        crearHistoriaEnanoF();
+    }else {
+        crearHistoriaEnanoO();
+    }
+}
+
+function crearHistoriaEnanoM(){
     texto.innerHTML = "";
     let nombre = document.getElementById('nombre').value.toString();
     if (!nombre){
@@ -224,3 +367,57 @@ function crearHistoriaEnano(){
     texto.innerHTML = historiaEnano.join(" ");
 }
 
+function crearHistoriaEnanoF(){
+    texto.innerHTML = "";
+    let nombre = document.getElementById('nombre').value.toString();
+    if (!nombre){
+        texto.innerHTML = "Por favor, introduce un nombre para nuestro heroina";
+        return;
+    }
+
+    let historiaEnano = [
+        "<h1>" + nombre + " La Martillo de Tharûm Khazad </h1>",
+        "En las minas de Tharûm Khazad, " + nombre + ", una enana de imponente barba rojiza,",
+        "forjaba armas imbuidas con runas ancestrales. Era hija de Tharin, el herrero más famoso de Erebor.",
+        "Su habilidad con el martillo era legendaria, pero su corazón anhelaba más que las profundidades de la roca.",
+        "Cuando un mensajero llegó desde Moria pidiendo ayuda contra los goblins, " + nombre + " no lo dudó.",
+        "Armada con su hacha runada y su escudo, lideró a un grupo de enanos hacia la Ciudad del Abismo.",
+        "En su viaje, enfrentaron numerosos peligros: lobos huargos, trampas ocultas y senderos traicioneros.",
+        "Al llegar a Moria, encontraron a sus hermanos atrapados en una gran sala, rodeados por cientos de goblins.",
+        "Con un grito de guerra que resonó como un trueno, " + nombre + " cargó al frente,",
+        "derribando a los goblins con cada golpe de su poderosa hacha.",
+        "En la batalla final, se enfrentó a una capitana goblin que llevaba un antiguo estandarte robado de los enanos.",
+        "Tras un feroz combate, " + nombre + " recuperó el estandarte y lo alzó victoriosa.",
+        "De regreso a Tharûm-Khazad, fue recibida como una heroína, pero para ella, el verdadero tesoro era la hermandad forjada en la batalla."
+    ];
+
+
+    texto.innerHTML = historiaEnano.join(" ");
+}
+
+function crearHistoriaEnanoO(){
+    texto.innerHTML = "";
+    let nombre = document.getElementById('nombre').value.toString();
+    if (!nombre){
+        texto.innerHTML = "Por favor, introduce un nombre para nuestre héroe";
+        return;
+    }
+
+    let historiaEnano = [
+        "<h1>" + nombre + " Le Martillo de Tharûm Khazad </h1>",
+        "En las minas de Tharûm Khazad, " + nombre + ", une enane de imponente barba rojiza,",
+        "forjaba armas imbuidas con runas ancestrales. Era hije de Tharin, el herrero más famoso de Erebor.",
+        "Su habilidad con el martillo era legendaria, pero su corazón anhelaba más que las profundidades de la roca.",
+        "Cuando un mensajero llegó desde Moria pidiendo ayuda contra los goblins, " + nombre + " no lo dudó.",
+        "Armade con su hacha runada y su escudo, lideró a un grupo de enanos hacia la Ciudad del Abismo.",
+        "En su viaje, enfrentaron numerosos peligros: lobos huargos, trampas ocultas y senderos traicioneros.",
+        "Al llegar a Moria, encontraron a sus hermanes atrapades en una gran sala, rodeades por cientos de goblins.",
+        "Con un grito de guerra que resonó como un trueno, " + nombre + " cargó al frente,",
+        "derribando a los goblins con cada golpe de su poderosa hacha.",
+        "En la batalla final, se enfrentó a une capitane goblin que llevaba un antiguo estandarte robado de los enanos.",
+        "Tras un feroz combate, " + nombre + " recuperó el estandarte y lo alzó victoriose.",
+        "De regreso a Tharûm-Khazad, fue recibide como une héroe, pero para elle, el verdadero tesoro era la hermandad forjada en la batalla."
+    ];
+
+    texto.innerHTML = historiaEnano.join(" ");
+}
